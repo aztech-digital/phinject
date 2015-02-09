3df74f61944a52b25b9ead26d1e9f4f03fc4b56e
Thibaud Fabre <thibaud@aztech.io>
2015-02-09 20:22:16 +0100

    Add new tests

--- tests/unit/DelegateContainerTest.php
+++ tests/unit/DelegateContainerTest.php
@@ -3,10 +3,31 @@
 namespace Aztech\Phinject\Tests;
 
 use Aztech\Phinject\ContainerFactory;
+use Aztech\Phinject\UnknownDefinitionException;
+
 class DelegateContainerTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function testLookupIsNotDelegatedToContainer()
+    public function testGetReturnsLocalEntriesButDelegatesDependencies()
+    {
+        $yml = <<<YML
+classes:
+    lookup:
+        class: \stdClass
+        properties:
+            test: @dependency
+YML;
+
+        $delegateContainer = $this->prophesize('\Interop\Container\ContainerInterface');
+        $delegateContainer->get('dependency')->willReturn(new \stdClass())->shouldBeCalled();
+
+        $container = ContainerFactory::createFromInlineYaml($yml);
+        $container->setDelegateContainer($delegateContainer->reveal());
+
+        $container->get('lookup');
+    }
+
+    public function testGetFallsbackToLocalLookupWhenDelegationFails()
     {
         $yml = <<<YML
 classes:
@@ -21,11 +42,34 @@ classes:
 YML;
 
         $delegateContainer = $this->prophesize('\Interop\Container\ContainerInterface');
-        $delegateContainer->get('dependency')->willReturn(new \stdClass())->shouldBeCalled();
+        $delegateContainer->get('dependency')->willThrow(new UnknownDefinitionException('dependency'));
 
         $container = ContainerFactory::createFromInlineYaml($yml);
         $container->setDelegateContainer($delegateContainer->reveal());
 
-        $container->get('lookup');
+        $lookup = $container->get('lookup');
+
+        $this->assertNotNull($lookup->test);
+        $this->assertInstanceOf('\stdClass', $lookup->test);
+    }
+
+    public function testHasReturnsLocalLookupResultAndDoesNotDelegate()
+    {
+        $yml = <<<YML
+classes:
+    lookup:
+        class: \stdClass
+        properties:
+            test: @dependency
+YML;
+
+        $delegateContainer = $this->prophesize('\Interop\Container\ContainerInterface');
+        $delegateContainer->has('missing')->shouldNotBeCalled();
+
+        $container = ContainerFactory::createFromInlineYaml($yml);
+        $container->setDelegateContainer($delegateContainer->reveal());
+
+        $this->assertTrue($container->has('lookup'));
+        $this->assertFalse($container->has('missing'));
     }
 }
