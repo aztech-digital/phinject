9642f380f56a237186038973f97f8d3ba9811e77
Thibaud Fabre <thibaud@aztech.io>
2015-02-12 17:33:13 +0100

    Add extra test for $name

--- tests/unit/ResolverSpecifications/ServiceNameResolverSpecificationTest.php
+++ tests/unit/ResolverSpecifications/ServiceNameResolverSpecificationTest.php
@@ -6,7 +6,7 @@ use Aztech\Phinject\ContainerFactory;
 class ServiceNameResolverSpecificationTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function testServiceNameIsInjectable()
+    public function testServiceNameIsInjectedIntoService()
     {
         $yaml = <<<YML
 classes:
@@ -20,5 +20,27 @@ YML;
         $service = $container->get('myNamedService');
 
         $this->assertEquals('myNamedService', $service->myName);
+    }
+
+    public function testDependencyServiceNameIsCorrectlyInjectedIntoDependency()
+    {
+        $yaml = <<<YML
+classes:
+    myNamedService:
+        class: \stdClass
+        properties:
+            myName: \$name
+            myDependency: '@myNamedDependency'
+    myNamedDependency:
+        class: \stdClass
+        properties:
+            myName: \$name
+YML;
+
+        $container = ContainerFactory::createFromInlineYaml($yaml);
+        $service = $container->get('myNamedService');
+
+        $this->assertEquals('myNamedService', $service->myName);
+        $this->assertEquals('myNamedDependency', $service->myDependency->myName);
     }
 }
